1ec86adda142c52a486a2094192ac0bb868f36b3
Sergio Gonzalez Requena <sergio.gonzalez.requena@devoteam.com>
2018-11-27 15:59:00 +0100

    Add saludo components

---
+++ src/app/home/saludo-local/saludo-local.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-saludo-local',
+  templateUrl: './saludo-local.component.html',
+  styleUrls: ['./saludo-local.component.css']
+})
+export class SaludoLocalComponent implements OnInit {
+  @ViewChild('nombreLoc') nombre: ElementRef;
+  
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.nombre);
+  }
+
+}
